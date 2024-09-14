bcc0f6b90d400cf4331c0e31c0aadbbe7231614b
Jonas <jonas.mokawane98@gmail.com>
2024-09-14 15:19:48 +0200

    Added data fetching and rendering to Products component, and updated background color in globals.css

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -1,7 +1,11 @@
+import Products from "../components/Products";
+
 export default function Home() {
   return (
     <div>
       <main>
+      <h1 className="text-center text-2xl font-bold mb-6">Welcome to Our Store</h1>
+         <Products />
       </main>
       <footer>
       </footer>
---
+++ my-next-app/src/components/Products.jsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function Products() {
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+      async function fetchProducts() {
+        let res = await fetch('https://next-ecommerce-api.vercel.app/products');
+        let data = await res.json();
+        setProducts(data);
+      }
+      fetchProducts();
+    }, []);
+  
+    if (!products) return <div>Loading...</div>;
+  
+    return (
+      <ul>
+        {products.map((products) => (
+          <li key={products.id}>
+            <img src={products.images} alt={products.title} />
+            <h2>{products.title}</h2>
+            <p>Category: {products.category}</p>
+            <p>Price: ${products.price}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
